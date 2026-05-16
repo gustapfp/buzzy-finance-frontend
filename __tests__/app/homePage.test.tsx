3c2d441f17b9a63b8968e4f27f25f2dd2df10d57
gustapfp <gustavopfp.trab@gmail.com>
2026-05-16 13:58:25 -0300

    fix: to use jest only and not bun test runner

--- __tests__/app/homePage.test.tsx
+++ __tests__/app/homePage.test.tsx
@@ -2,7 +2,6 @@ import { render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import HomePage from "../../app/page";
 import "../../lib/i18n";
-import { InConstruction } from "../../components/shared/inContruction/inContruction";
 import { I18nProvider } from "../../components/18nProvider/I18nProvider";
 
 describe("Home page", () => {

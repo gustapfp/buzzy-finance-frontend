c6868ef330926a828d76a0685980be46d047c9c7
gustapfp <gustavopfp.trab@gmail.com>
2026-05-17 17:50:15 -0300

    feat: helth check tests

--- tests/integration/api/v1/health/getHealth.test.ts
+++ tests/integration/api/v1/health/getHealth.test.ts
@@ -2,9 +2,16 @@ const BASE_URL = "http://localhost:3000/api";
 
 describe("API -> Health Check", () => {
   describe("GET /v1/health", () => {
-    it("returns 200", async () => {
+    it("returns 200 on API call", async () => {
       const response = await fetch(`${BASE_URL}/v1/health`);
       expect(response.status).toBe(200);
     });
+    it("returns 404 when called wrong API version", async () => {
+      let response = await fetch(`${BASE_URL}/helth`)
+      expect(response.status).toBe(404)
+      response = await fetch(`${BASE_URL}/v0/helth`)
+      expect(response.status).toBe(404)
+
+    })
   });
 });

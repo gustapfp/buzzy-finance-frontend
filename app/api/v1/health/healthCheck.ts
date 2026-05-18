fd26a9006610158a542f03e92561e04fb93805cd
gustapfp <gustavopfp.trab@gmail.com>
2026-05-18 14:37:40 -0300

    fix: correct import path for InConstruction component and enhance health check to include PostgreSQL status

--- app/api/v1/health/healthCheck.ts
+++ app/api/v1/health/healthCheck.ts
@@ -1,4 +1,18 @@
-const healthCheck = () => {
-  return Response.json({ status: "ok", message: "Aplication running..." }, { status: 200 });
+import { databaseHealthCheck } from "../../../../infra/database";
+
+const healthCheck = async () => {
+  const postgreSQLCheck = await databaseHealthCheck();
+  return Response.json(
+    {
+      status: "ok",
+      message: {
+        nodeServer: "Application running...",
+        postgreSQL: postgreSQLCheck.db_response,
+      },
+    },
+    {
+      status: postgreSQLCheck.exit_code === 0 ? 200 : 500,
+    },
+  );
 };
 export default healthCheck;

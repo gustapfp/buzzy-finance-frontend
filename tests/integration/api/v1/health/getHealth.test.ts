d7287e59888b56a8c0d9e0942dc7e82363085c73
gustapfp <gustavopfp.trab@gmail.com>
2026-05-19 16:11:57 -0300

    chore: update VSCode settings aand enhance health check tests for helthcheck endpoint

--- tests/integration/api/v1/health/getHealth.test.ts
+++ tests/integration/api/v1/health/getHealth.test.ts
@@ -12,13 +12,16 @@ describe("API -> Health Check", () => {
       const response = await fetch(`${BASE_URL}/v1/health`);
       const data = (await response.json()) as HealthCheckResponse;
 
+      expect(data.database.postgres_version).toBe("V16.0");
       expect(data.webapp.server_message).toBe("Application running...");
       expect(data.database.db_message).toBe("Database connection ok...");
-      expect(data.database.active_connections).toEqual(expect.any(Number));
+      expect(data.database.active_connections).toEqual(1);
       expect(data.database.max_connections).toEqual(expect.any(Number));
+      expect(data.database.max_connections).toBeLessThanOrEqual(100);
       expect(data.database.update_at).toBeTruthy();
       expect(new Date(data.database.update_at).toISOString()).toBe(data.database.update_at);
     });
+
     it("returns 404 when called wrong API version", async () => {
       let response = await fetch(`${BASE_URL}/health`);
       expect(response.status).toBe(404);
